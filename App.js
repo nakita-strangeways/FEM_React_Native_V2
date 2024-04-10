e3b3222a6351fe2a9ebd58f2d81240c7659060f3
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2024-04-10 12:28:02 -0700

    adding useEffects and Modal

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./screens/Home";
-import ColorPalette from "./screens/ColorPalette";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPalette';
+import AddNewPalette from './screens/AddNewPalette';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
+const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        name="ColorPalette"
+        component={ColorPalette}
+        options={({ route }) => ({ title: route.params.paletteName })}
+      />
+    </MainStack.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* Top stack -> root and will default show first */}
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="ColorPalette"
-          component={ColorPalette}
-          options={({ route }) => ({ title: route.params.paletteName })}
+      <RootStack.Navigator screenOptions={{ presentation: 'modal' }}>
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
         />
-      </Stack.Navigator>
+        <RootStack.Screen name="AddNewPalette" component={AddNewPalette} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
